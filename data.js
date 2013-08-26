6d2818ee3e3bc34d5108b72368e75b47f82272d9
Seth Howard <smiles.seth@gmail.com>
2013-08-26 15:22:00 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,55 @@
+window.data1 = [
+  {
+    title: 'Website visits',
+    value: 15654,
+    color: 'green'
+  },
+  {
+    title: 'Downloads',
+    value: 4064,
+    color: 'yellow'
+  },
+  {
+    title: 'Requested price list',
+    value: 1987,
+    color: 'orange'
+  },
+  {
+    title: 'Invoice sent',
+    value: 976,
+    color: 'black'
+  },
+  {
+    title: 'Finalized',
+    value: 846,
+    color: 'red'
+  }
+];
+
+window.data2 = [
+  {
+    title: 'Website visits',
+    value: 100,
+    color: 'green'
+  },
+  {
+    title: 'Downloads',
+    value: 90,
+    color: 'yellow'
+  },
+  {
+    title: 'Requested price list',
+    value: 85,
+    color: 'orange'
+  },
+  {
+    title: 'Invoice sent',
+    value: 25,
+    color: 'black'
+  },
+  {
+    title: 'Finalized',
+    value: 10,
+    color: 'red'
+  }
+];
